c337e90600745fe839267dad9e5647a9c010867c
bryan292 <bryan292@outlook.com>
2024-08-23 12:47:56 -0600

    chore: Update server code and package.json

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const http = require('http');
-const hostname = '0.0.0.0'; // changed from localhost to 0.0.0.0
+const hostname = '0.0.0.0';
 const port = 3000;
 const appEnv = process.env.APP_ENV || 'No env specified';
+
+console.log('Starting server...');
+
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
+    console.log('Received request:', req.headers);
     res.statusCode = 200;
-    res.end(`<html><body><h1>Hello, World ${appEnv}</h1></body></html>`); // quoation fixed by adding backticks
-})
-server.listen(port, hostname);
+    res.end(`<html><body><h1>Hello, World ${appEnv}</h1></body></html>`);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
